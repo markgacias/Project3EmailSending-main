064c25a3ec5e84932959a72814616353880d578f
markgacias <markgacias@gmail.com>
2021-04-30 19:52:11 +0800

    Validated the fields

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 //Variables
-const sendBtn = document.getElementById('sendBtn');
-
+const sendBtn = document.getElementById('sendBtn'),
+      email = document.getElementById('email'),
+      subject = document.getElementById('subject'),
+      message = document.getElementById('message');
 
 
 //Event Listeners
@@ -11,6 +13,11 @@ function EventListeners()
 {
     //App Init
     document.addEventListener('DOMContentLoaded', appInit);
+
+    //Validate the forms
+    email.addEventListener('blur', validateField);
+    subject.addEventListener('blur', validateField);
+    message.addEventListener('blur', validateField);
 }
 
 
@@ -22,3 +29,49 @@ function appInit()
     //disable the send button on load
     sendBtn.disabled = true;
 } 
+
+//Validate the fields
+function validateField()
+{
+    let errors;
+
+    //validate the lenght of the field
+    validateLength(this)
+
+    //Validate the email
+    if(this.type === 'email')
+    {
+        validateEmail(this);
+    }
+}
+
+//Validate the lenght of the fields
+function validateLength(field)
+{
+    if(field.value.length > 0)
+    {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    }
+    else
+    {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
+}
+//Validate email (checks for @ in the value)
+function validateEmail(field)
+{
+    let emailText = field.value;
+    //check if the email contains the @ sign
+    if(emailText.indexOf('@') !== -1)
+    {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    }
+    else
+    {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
+}
